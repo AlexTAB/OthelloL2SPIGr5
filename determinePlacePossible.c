43c9071c33e5d0fe09f7edc7127ff9ef29bd3115
AlexTAB <ccbalexandretabot@orange.fr>
2015-11-19 09:18:01 +0100

    push de demi seance 19 nov 2015 AT

---
+++ determinePlacePossible.c
@@ -0,0 +1,94 @@
+// 
+// Alexandre TABOT L2 SPI Université du Mans 2015-2016
+// Date :
+// Compilation : exemple :alexandre@alexandre-tabot7:~/Documents/pbc$ gcc affiche var ex001.c -o afficheEx001 -Wall
+// Execution :
+//
+
+//note
+	
+// 0 -> vid 
+// Inclusion des bibliothèques
+#include <stdio.h> 
+#include <stdlib.h>
+
+// Déclaration des fonctions et des procédures du programmes
+void exemple_fonction(int a, char c)
+{
+	// Modification et renvoie :
+	
+	// Déclaration des variables de la fonction
+	int i , j , k, n, y;
+	int tab[100];
+	char table[50];
+	float x;
+	int resultat;
+	int compteur;
+	char choix;
+
+	// Initialisation des variables de la fonction
+	i = 0;
+	j = 0;
+
+	// Corps de la fonction
+	for (i=0 ; i < N ; i++)
+	{
+		
+		if (plateau[i][j] == 'X') 
+	{
+		// condition :
+		somme_lig_j1++;
+		somme_lig_
+	}
+		// boucle parcours ligne tableau
+ 		for (j=0 ; j < M ; j++)
+		{
+			// boucle parcours colonne tableau
+		if (choix == 'y') 
+	{
+		// condition :
+	}	printf(" %c ", plateau[i][j]);
+		}
+		printf("\n");
+	}
+	
+ 
+	else {
+
+	}
+	do
+	{
+		//boucle :
+	}
+	while (n < k);
+
+	while (resultat < n)
+	{
+	//boucle : 
+
+		resultat++;
+	}
+
+void determiner_gagnant(char plateau[M][N])
+{
+	// Modification et renvoie : modifie la grille avec l'insertion d'un X ou O, selon quel utilisateur saisit Les coordonnées
+	// de la cellule puis le numero du joueur sont passés en parametres
+	
+	// Déclaration des variables de la fonction
+	int i , j;
+	int tab[100];
+	char table[50];
+	float x;
+	int nb_case_aligne_j2 = 0;
+	int nb_case_aligne_j1 = 0;
+	char choix;
+
+	// Initialisation des variables de la fonction
+	i = 0;
+	j = 0;
+
+	// Corps de la fonction
+	
+
+}
+
