a478c4494703cc98e962106890e2fde8b9896ce3
AlexTAB <ccbalexandretabot@orange.fr>
2015-11-19 10:16:16 +0100

    push de fin seance 19 nov 2015 AT

--- determinePlacePossible.c
+++ determinePlacePossible.c
@@ -6,89 +6,86 @@
 //
 
 //note
-	
-// 0 -> vid 
+//	valeurs de case matrice grille
+// 0 -> vide
+// 1-> noir : pion du joueur 1 j1
+// 2-> blanc: pion du joueur 2 j2 
+// 3 -> possible
+// 4-> plus
+
 // Inclusion des bibliothèques
 #include <stdio.h> 
 #include <stdlib.h>
 
 // Déclaration des fonctions et des procédures du programmes
-void exemple_fonction(int a, char c)
+void determinePlacePossible(int a, char grille[M][N])
 {
 	// Modification et renvoie :
 	
 	// Déclaration des variables de la fonction
-	int i , j , k, n, y;
-	int tab[100];
-	char table[50];
-	float x;
-	int resultat;
-	int compteur;
+	// variables compteur
+	int i , j;
+	// variable communes
+	
+	// variables propres à la fonction
+	int i_somme-lig-j1;
+	int i_somme-lig-j2;
+	float f_x;
+	int i_somme-col-j1;
+	int i_somme-col-j2;
 	char choix;
 
 	// Initialisation des variables de la fonction
 	i = 0;
 	j = 0;
+	i_somme-lig-j1 =0;
+	i_somme-col-j1=0;
+	i_somme-col-j2=0;
 
 	// Corps de la fonction
 	for (i=0 ; i < N ; i++)
-	{
-		
-		if (plateau[i][j] == 'X') 
-	{
-		// condition :
-		somme_lig_j1++;
-		somme_lig_
-	}
-		// boucle parcours ligne tableau
- 		for (j=0 ; j < M ; j++)
 		{
-			// boucle parcours colonne tableau
-		if (choix == 'y') 
-	{
-		// condition :
-	}	printf(" %c ", plateau[i][j]);
+			//Assert 1 dans determinePlacePossible pour une boucle For
+			if (grille[i][j] == 1)  {
+				// condition : si trouve un pion noir
+				// Assert 2 dans determinePlacePossible pour une condition IF
+				somme_lig_j1++;
+				i_somme_lig_vide = 0;
+			}
+			if (grille[i][j] == 2) {
+				// condition : si trouve un pion blanc
+				// Assert 3 dans determinePlacePossible pour une condition IF
+				somme_lig_j2++;
+				i_somme_lig_vide = 0;
+			}
+			//
+ 			for (j=0 ; j < M ; j++)
+			{
+				// boucle parcours colonne tableau
+				//Assert 4 dans determinePlacePossible pour une boucle For
+				
+				if (grille[i][j] == 1)  {
+					// condition : si trouve un pion noir
+					// Assert 5 dans determinePlacePossible pour une condition IF
+					somme_col_j1++;
+				i_somme_col_vide = 0;
+				}
+				if (grille[i][j] == 2) {
+					// condition : si trouve un pion blanc
+					// Assert 6 dans determinePlacePossible pour une condition IF
+					somme_col_j2++;
+					i_somme_col_vide = 0;
+				}
+	            // etape valeur case grille 0, 3 ou 4. Ceci ne nécessite pas de sommage des précédntes pour mémorisation.
+	            // Ces sommes sont nécessaires dans les condition de determination.
+	            if (grille[i][j] == 0) {
+					// condition : si trouve une case vide 
+					// Assert 6 dans determinePlacePossible pour une condition IF
+					somme_col_j2++;
+					i_somme_col_vide = 0;
+				}
+			}
 		}
-		printf("\n");
-	}
-	
- 
-	else {
-
-	}
-	do
-	{
-		//boucle :
-	}
-	while (n < k);
-
-	while (resultat < n)
-	{
-	//boucle : 
-
-		resultat++;
 	}
 
-void determiner_gagnant(char plateau[M][N])
-{
-	// Modification et renvoie : modifie la grille avec l'insertion d'un X ou O, selon quel utilisateur saisit Les coordonnées
-	// de la cellule puis le numero du joueur sont passés en parametres
-	
-	// Déclaration des variables de la fonction
-	int i , j;
-	int tab[100];
-	char table[50];
-	float x;
-	int nb_case_aligne_j2 = 0;
-	int nb_case_aligne_j1 = 0;
-	char choix;
-
-	// Initialisation des variables de la fonction
-	i = 0;
-	j = 0;
-
-	// Corps de la fonction
-	
-
-}
 
