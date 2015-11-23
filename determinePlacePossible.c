0803d56d378058a08a83052081d2994b33887786
AlexTAB <ccbalexandretabot@orange.fr>
2015-11-23 15:12:17 +0100

    Push de demi seance 23 nov 2015 sur fonction determinePlacePossible AT

--- determinePlacePossible.c
+++ determinePlacePossible.c
@@ -18,13 +18,14 @@
 #include <stdlib.h>
 
 // Déclaration des fonctions et des procédures du programmes
-void determinePlacePossible(int a, char grille[M][N])
+void determinePlacePossible(int a; char grille[M][N])
 {
 	// Modification et renvoie :
 	
 	// Déclaration des variables de la fonction
 	// variables compteur
 	int i , j;
+	
 	// variable communes
 	
 	// variables propres à la fonction
@@ -43,49 +44,75 @@ void determinePlacePossible(int a, char grille[M][N])
 	i_somme-col-j2=0;
 
 	// Corps de la fonction
-	for (i=0 ; i < N ; i++)
-		{
-			//Assert 1 dans determinePlacePossible pour une boucle For
+	for (i=0 ; i < N ; i++) {
+		//Assert 1 dans determinePlacePossible pour une boucle For
+		if (grille[i][j] == 1)  {
+			// condition : si trouve un pion noir
+			// Assert 2 dans determinePlacePossible pour une condition IF
+			somme_lig_j1++;
+			i_somme_lig_vide = 0;
+		}
+		if (grille[i][j] == 2) {
+			// condition : si trouve un pion blanc
+			// Assert 3 dans determinePlacePossible pour une condition IF
+			somme_lig_j2++;
+			i_somme_lig_vide = 0;
+		}
+		//
+ 		for (j=0 ; j < M ; j++) {
+			// boucle parcours colonne tableau
+			//Assert 4 dans determinePlacePossible pour une boucle For
+				
 			if (grille[i][j] == 1)  {
 				// condition : si trouve un pion noir
-				// Assert 2 dans determinePlacePossible pour une condition IF
-				somme_lig_j1++;
-				i_somme_lig_vide = 0;
+				// Assert 5 dans determinePlacePossible pour une condition IF
+				somme_col_j1++;
+				i_somme_col_vide = 0;
 			}
 			if (grille[i][j] == 2) {
 				// condition : si trouve un pion blanc
-				// Assert 3 dans determinePlacePossible pour une condition IF
-				somme_lig_j2++;
-				i_somme_lig_vide = 0;
-			}
-			//
- 			for (j=0 ; j < M ; j++)
-			{
-				// boucle parcours colonne tableau
-				//Assert 4 dans determinePlacePossible pour une boucle For
-				
-				if (grille[i][j] == 1)  {
-					// condition : si trouve un pion noir
-					// Assert 5 dans determinePlacePossible pour une condition IF
-					somme_col_j1++;
+				// Assert 6 dans determinePlacePossible pour une condition IF
+				somme_col_j2++;
 				i_somme_col_vide = 0;
-				}
-				if (grille[i][j] == 2) {
-					// condition : si trouve un pion blanc
-					// Assert 6 dans determinePlacePossible pour une condition IF
-					somme_col_j2++;
-					i_somme_col_vide = 0;
-				}
-	            // etape valeur case grille 0, 3 ou 4. Ceci ne nécessite pas de sommage des précédntes pour mémorisation.
-	            // Ces sommes sont nécessaires dans les condition de determination.
-	            if (grille[i][j] == 0) {
-					// condition : si trouve une case vide 
-					// Assert 6 dans determinePlacePossible pour une condition IF
-					somme_col_j2++;
-					i_somme_col_vide = 0;
-				}
+			}
+			// etape valeur case grille 0, 3 ou 4. Ceci nécessite mise des somme case pion des précédntes pour mémorisation.
+			// Ces sommes sont nécessaires dans les condition de determination.
+			if (grille[i][j] == 0) {
+				// incrémentation des sommes de cases vides
+				i_somme_col_vide++;
+				i_somme_lig_vide++;
+				// condition : si trouve une case vide 
+				// Assert 7 dans determinePlacePossible pour une condition IF
+				if (((somme_col_j2 >0) && (somme_col_j1 >0)) || ((somme_lig_j2 >0) && (somme_lig_j1 >0)) {
+					// condition :  si trouve case vide et qu' il ya une somme de case pion sans case vide alors devient case possible
+					grille[i][j] = 3;
+					//mise à zéro des somme de case pion
+					somme_col_j2 =0;
+					somme_col_j1 =0;
+					somme_lig_j2 =0;
+					somme_lig_j1 =0;
+				}					
+			}
+			if (grille[i][j] == 3) {
+				// les case de type 3 "possible" sont aussi des cases vides, donc même traitement des sommes de type de cases
+				i_somme_col_vide++;
+				i_somme_lig_vide++;
+				somme_col_j2 =0;
+				somme_col_j1 =0;
+				somme_lig_j2 =0;
+				somme_lig_j1 =0;
+			}
+			if (grille[i][j] == ) {
+				// les case de type 4 "plus" sont aussi des cases vides, donc même traitement des sommes de type de cases
+				i_somme_col_vide++;
+				i_somme_lig_vide++;
+				somme_col_j2 =0;
+				somme_col_j1 =0;
+				somme_lig_j2 =0;
+				somme_lig_j1 =0;
 			}
 		}
 	}
+}
 
 
