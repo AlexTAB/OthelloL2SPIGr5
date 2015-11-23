1cd8e32fa285f9fd0bb2366ad5e53cadc70e574e
murdo nicolai <spi2136@ic2s122-05.univ-lemans.fr>
2015-11-23 15:36:46 +0100

    terminé les code scores et gestion de fichier

---
+++ GestionFichier.c
@@ -0,0 +1,99 @@
+#include<stdio.h>
+#define N 8
+#define M 8
+
+int grille[N][M] = {{0,0,0,0,2,2,2,2},{0,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1},{0,0,2,2,0,0,0,0},{0,1,2,0,0,0,0,0},{0,0,0,1,1,0,0,0},{0,0,0,2,0,0,0,0},{0,0,0,0,2,2,2,2}};
+
+
+
+void afficher(){
+    int i,j;
+    for(i=0;i<N;i++){
+        printf("\n");
+        for(j=0;j<M;j++){
+            if (grille[i][j]==0)
+                printf("| |");
+            else if (grille[i][j]==1)
+                printf("|+|");
+            else if (grille[i][j]==2)
+                printf("|-|");
+        }
+    }
+}
+
+
+void sauvegarde(){//on met la grille dans un fichier entrer par l'utilisateur
+    FILE * fic;
+    int bSupfic=0;
+    int iDemande=0;
+    char sNom[20];
+    
+    
+    while (!bSupfic){ //ici sont les option qui vérifie que l'utilisateur sauvegarde bien ou il veut sans suprimer des sauvegardes si il ne voulait pas.
+        printf("entrez le nom de votre sauvegarde:  ");
+        scanf("%s",sNom);
+        if (fopen(sNom,"r")!=NULL){
+            printf("cette sauvegarde existe déjé voulez vous la supprimer? \n1:oui\n2:non\n");
+            while(iDemande!=1 && iDemande!=2){
+                printf("entrez 1 ou 2\n");
+                scanf("%i",&iDemande);
+            }
+            if (iDemande==1)
+                bSupfic=1;
+            else
+                bSupfic=0;
+
+        }
+        else{
+            bSupfic=1;
+        }
+    }
+    if (bSupfic==1){
+		fic = fopen(sNom,"w");
+    
+
+		int i,j;//on met les données de la grille dans la sauvegarde
+		for(i=0;i<N;i++){
+			for(j=0;j<M;j++){
+				fprintf(fic,"%i ",grille[i][j]);
+			}
+		}
+		printf("sauvegarde réussie \n");
+		fclose(fic);
+	}
+}
+
+
+void chargement(){//on charge un fichier que l'utilisateur à choisit
+    FILE * fic;
+    char sNom[20]={0};
+    
+    do{//verification si sauvegarde existe
+		printf("quelle sauvegarde voulez vous charger?\n");   
+		scanf("%s",sNom);
+		fic = fopen(sNom,"r");
+        if (fic==NULL)
+			printf("cette sauvegarde n'existe pas\n");
+			
+    }while(fic==NULL);
+    
+    int i,j;//on met dans la grille les données de la sauvegarde
+    for(i=0;i<N;i++){
+        for(j=0;j<M;j++){
+            fscanf(fic,"%i ",&grille[i][j]);
+        }
+    }
+    fclose(fic);
+        
+}
+    
+        
+        
+        
+    int main(){
+		afficher();
+        chargement();
+        afficher();
+        
+        return 0;
+    }
