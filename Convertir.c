ec9741ffa432053e9802a47fb0d9596748dc5cd4
WANG/Weibin <aini19931225@hotmail.com>
2015-11-26 12:11:54 +0100

    La fonction de changer couleur

--- Convertir.c
+++ Convertir.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<stdlib.h>
+
 
 #define N 8
 typedef enum {vide, blanc, noire, possible, le_plus} pion;
@@ -13,16 +15,17 @@ int nombre_noireAUTRE=0;
 void initgrille(int matrice[N][N])
 {
 	int i,j;
-	for(i=0;i<N;i++)
-		for(j=0;j<N;j++)
+	for(i=0;i<=N;i++)
+		for(j=0;j<=N;j++)
 			matrice[i][j]=vide;
+        matrice[2][2]=blanc;
 	matrice[2][3]=blanc;
 	matrice[3][3]=noire;
 	matrice[3][4]=noire;
 	matrice[4][3]=noire;
-	matrice[4][4]=blanc;
 	matrice[4][2]=noire;
 	matrice[3][2]=blanc;
+	matrice[1][1]=blanc;
 }
 void affiche(int matrice[N][N])
 {
@@ -35,135 +38,82 @@ void affiche(int matrice[N][N])
                 }       
 }
 /*de colone chercher les même couleur et entre deux même couleur il y a au minimum 1 pion diffirent, on peut changer les couleurs entre deux mêmes pions */
-int path_col(int matrice[N][N],int i,int j)
+	
+int path_diago_A(int matrice[N][N],int i,int j, int dx, int dy)
 {
-        int col;int lig;
-        int marque_nb_blanc=1;
-        int marque_nb_noire=1;
-        for(lig=0;lig<N;lig++)
-        {
-                if(lig<i)
-                {
-                        if(matrice[lig][j]==blanc)
-                                nombre_blanc++;
-                        else if(matrice[lig][j]==noire)
-                                nombre_noire++;// la côté sur (i,j)
-                }
-                
-                if(lig>i&&lig<N)
-                {
-                        if(matrice[lig][j]==blanc)
-                                nombre_blancAUTRE++;
-                        else if(matrice[lig][j]==noire)
-                                nombre_noireAUTRE++; // la côté desous (i,j)       
-                }
-        }
-        //test
-        printf("%i %i %i %i \n",nombre_blanc,nombre_noire,nombre_blancAUTRE,nombre_noireAUTRE);
-        if(matrice[i][j]==blanc && nombre_blancAUTRE>0)//entre deux pions blancs
-        {
-                for(lig=N-1;lig>i;lig--)
-                {
-                	if(matrice[lig][j]==blanc)
-                		if(matrice[lig-1][j]==noire)
-                			marque_nb_blanc++;
-                	if(marque_nb_blanc>=2)
-                        matrice[lig][j]=blanc;
-                }
-        }
-        if(matrice[i][j]==blanc && nombre_blanc>0)
-        {
-                for(lig=0;lig<i;lig++)
-                	{
-                	if(matrice[lig][j]==blanc)
-                		if(matrice[lig+1][j]==noire)
-                		marque_nb_blanc++;
-                	if(marque_nb_blanc>=2)
-                        matrice[lig+1][j]=blanc;
-                    }
-        }
-        else if(matrice[i][j]==noire && nombre_noireAUTRE>0)//entre deux pions noires
-        {
-                for(lig=N-1;lig>i;lig--)
-                {
-                	if(matrice[lig][j]==noire)
-                		if(matrice[lig-1][j]==blanc)
-                			marque_nb_noire++;
-                	if(marque_nb_noire>=2)
-                        matrice[lig][j]=noire;
-                }
-        }
-        else if(matrice[i][j]==noire && nombre_noire>0)
-        {
-        	for(lig=0;lig<i;lig++)
-        		{
-        			if(matrice[lig][j]==noire)
-        				if(matrice[lig+1][j]==blanc)
-        					marque_nb_noire++;
-        			if(marque_nb_noire>=2)
-        				matrice[lig+1][j]=noire;
-        		}
-        }
-        return 0;
+	int icur,jcur;
+	icur=i;
+	jcur=j;
+	//fprintf(stderr, "On joue en (%d,%d)\n", i, j);
+	//fprintf(stderr,"On garde la direction(%d,%d)",dx,dy);
+	if(matrice[i][j]==blanc) // la case courante est blanche, on cherche les pions noirs
+	{
+		do
+		{
+			icur+=dx;
+			jcur+=dy;
+			//fprintf(stderr, "On regarde la case (%d,%d) qui vaut %d \n", icur, jcur,matrice[icur][jcur] );
+			 
+			}
+		while(matrice[icur][jcur]==noire);
+		//fprintf(stderr, "On s'arrete sur la case (%d,%d)\n", icur, jcur);
+		
+		// (icur,jcur) est une case non noire (soir vide soit blanche)
+		// si elle est vide -> on ne fait rien
+		// si elle est blanche : on retourne les pions entre (i,j) et (icur, jcur)
+		if(matrice[icur][jcur] == blanc){
+			icur -= dx; jcur -= dy;
+			//fprintf(stderr,"Examine seconde (%d,%d)\n",icur,jcur);
+			while(icur != i || jcur!=j){
+				//fprintf(stderr, "On retourne la case (%d,%d)\n", icur, jcur);
+				matrice[icur][jcur] = blanc;
+				icur -= dx; jcur -= dy;
+			}
+		
+		}
+		
+	}
+	return 1;
 }
 
-int path_lig(int matrice[N][N],int i,int j)
-{
-	int lig,col;
-	int marque_nb_pion=1;
-	for(col=0;col<N;col++)
-		{
-			if(col<j)//à gouche
-			{
-				if(matrice[i][col]==blanc)
-					nombre_blanc++;
-					else if(matrice[i][col]==noire)
-					nombre_noire++;
-			}
-			else if(col>j&&col<N)//à droite
-			{
-				if(matrice[i][col]==noire)
-					nombre_noireAUTRE++;
-					else if(matrice[i][col]==blanc)
-					nombre_blancAUTRE++;
-			}
-		}
-	for(col=0;col<j;col++)
-		{
-			if(matrice[i][col]==matrice[i][j]==blanc && nombre_blanc>0)
-				if(matrice[i][col+1]==noire)
-					matrice[i][col+1]=blanc;
-			if(matrice[i][col]==matrice[i][j]==noire && nombre_noire>0)
-				if(matrice[i][col+1]==blanc)
-					matrice[i][col+1]=noire;
-		}
-	for(col=N-1;col>j;col--)
-	{
-		if(matrice[i][col]==matrice[i][j]==blanc && nombre_blancAUTRE>0)
-			if(matrice[i][col-1]==noire)
-				matrice[i][col-1]=blanc;
-		if(matrice[i][col]==matrice[i][j]==noire && nombre_noireAUTRE>0)
-			if(matrice[i][col-1]==blanc)
-				matrice[i][col-1]=noire;
-	}
+int path_tous(int matrice[N][N],int i,int j){
+	int dx,dy;
+	for(dx=-1; dx<=1; dx++)
+		for(dy=-1; dy<=1; dy++)
+			if(dx!=0 || dy!=0)
+				path_diago_A(matrice,i,j, dx, dy);
+	
 	return 0;
-}
-int convertir(int matrice[N][N],int i, int j)
-{
-          return 0;      
 }
 
 int main()
 {
+	/*puts("tset");
+	printf("%d\n",judge_dia(1,1,4,3));
+	printf("%d\n",judge_dia(4,4,1,1));
+	printf("%d\n",judge_dia(4,4,6,2));
+	puts("test");
+	*/
+	
 	initgrille(grille);
 	affiche(grille);
 	int col,lig;
 	puts("Please input your pion");
+	
+	while(1)
+	{
 	scanf("%i%i",&lig,&col);
 	grille[lig][col]=blanc;
 	affiche(grille);
-	path_col(grille,lig,col);
-	path_lig(grille,lig,col);
+	puts("");
+	//path_col(grille,lig,col);
+	//path_lig(grille,lig,col);
+	path_tous(grille,lig,col);
 	affiche(grille);
+	scanf("%d%d",&lig,&col);
+	puts("");
+	grille[lig][col]=noire;
+	affiche(grille);
+	}
 	return 0;
 }
