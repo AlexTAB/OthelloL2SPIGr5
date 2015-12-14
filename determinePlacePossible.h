c29731ddb9aa6cdb5e66c8e93a301923d9e4b27f
WANG/Weibin <aini19931225@hotmail.com>
2015-12-14 16:24:33 +0100

    modified:   determinePlacePossible.h

--- determinePlacePossible.h
+++ determinePlacePossible.h
@@ -5,6 +5,7 @@
 //  Created by Nicolai Murdo on 07/12/2015.
 //
 //
+
 void determinePlacePossible(int joueur);
 void verification(int k,int *vertical,int *horrisontal);
-void enlever3();
+void enlever3(pion);
